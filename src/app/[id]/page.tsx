fa14a01ae689d91777ed0952ef1e09a7e1732516
Ub207 <usmaanubaidurrehman@gmail.com>
2024-12-27 03:49:57 +0500

    assignment done

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,18 +1,18 @@
-const DynamicPhoto = async (props: any) => {
+const DynamicUser = async (props: any) => {
 	
 	const url = await fetch(
-		`https://jsonplaceholder.typicode.com/photos/${props.params.id}`
+		`https://jsonplaceholder.typicode.com/users/${props.params.id}`
 	);
 	const res = await url.json();
-	console.log("SingleAlbumREsponse", res);
+	console.log("SingleUserREsponse", res);
 
 	return (
 		<h1 className='pl-5 text-5xl'>
 			{props.params.id} <br />
-			{res.photo} <br />
+			{res.name} <br />
 			{res.author} <br />
 		</h1>
 	);
 };
 
-export default DynamicPhoto;
+export default DynamicUser;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
-
+import Link from "next/link";
 
 export default async function Home() {
-
-  const url = await fetch('https://jsonplaceholder.typicode.com/photos');
-
-  const response = await url.json()
-  console.log(response);
-  
-  return (
-      <main>
-         {response.map((photo:any,index : number) =>(
-          <div key = {index}>
-
-              <h1 className="text-6xl">
-                  {photo.id}
-                  {photo.name}
-              </h1>
-
-          </div>
-         ))} 
-      </main>
-
-
-);
+	const url = await fetch("https://jsonplaceholder.typicode.com/users");
+	const res = await url.json();
+	console.log(res);
+	return (
+		<main>
+			{res.map((user: any, index: number) => (
+				<div key={index}>
+					<Link href={`${user.id}`}>
+						<h1 className='text-6xl'>
+							{user.id} &nbsp; &nbsp;
+							{user.name}
+						</h1>
+					</Link>
+				</div>
+			))}
+		</main>
+	);
 }
